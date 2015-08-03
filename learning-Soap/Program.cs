50036c2d78086e47257b376037059adbf31dc98c
Eedrah <eedrah@users.noreply.github.com>
2015-08-03 16:26:41 +1200

    create SOAP hello world

---
+++ learning-Soap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_Soap {
+    class Program {
+        static void Main(string[] args) {
+            var http = new TestDecisionLogic.IntegrationSoapClient();
+            Console.WriteLine(http.HelloWorld());
+            Console.WriteLine(http.HelloWorld_SSLOnly());
+
+            var https = new testHTTPS.IntegrationSoapClient();
+            Console.WriteLine(https.HelloWorld_SSLOnly());
+
+            Console.ReadKey();
+        }
+    }
+}
